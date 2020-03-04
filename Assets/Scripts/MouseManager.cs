eb9d8c11ebad5607cc23257a80647510a5865bac
Nastaliss <stanbill91@gmail.com>
2020-03-04 16:53:29 +0100

    Add interactions

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -11,26 +11,58 @@ public class MouseManager : MonoBehaviour
     public Image ArrowRight;
     public Image ArrowDown;
     public Image ArrowLeft;
+
     void Start()
     {
-        HideArrow(ArrowUp);
-        HideArrow(ArrowRight);
-        HideArrow(ArrowDown);
-        HideArrow(ArrowLeft);
+        // HideArrow("up");
+        HideArrow("right");
+        HideArrow("down");
+        HideArrow("left");
+        IndicateArrow("up");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void ShowArrow(Image Arrow) {
-        Arrow.enabled = true;
+    private Image GetArrow(string ArrowIndicator) {
+        switch (ArrowIndicator) {
+            default:
+            case "up":
+                return ArrowUp;
+            case "right":
+                return ArrowRight;
+            case "down":
+                return ArrowDown;
+            case "left":
+                return ArrowLeft;
+        }
+
+    }
+    public void ShowArrow(string ArrowDirection) {
+        GetArrow(ArrowDirection).enabled = true;
     }
 
-    public void HideArrow(Image Arrow) {
-        Arrow.enabled = false;
+    public void HideArrow(string ArrowDirection) {
+        GetArrow(ArrowDirection).enabled = false;
+    }
+
+    public void IndicateArrow(string ArrowDirection) {
+        StartCoroutine(Indicate(GetArrow(ArrowDirection), 2f));
+    }
+
+    IEnumerator Indicate(Image Arrow, float TimeSeconds) {
+        var endTime=Time.time + TimeSeconds;
+        Debug.Log(Time.time);
+        Debug.Log(endTime);
+        while(Time.time < endTime) {
+            Arrow.color = new Color(1f, 0f, 0f);
+            yield return new WaitForSeconds(0.2f);
+            Arrow.color = new Color(0f, 0f, 0f);
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 
 }
